5b29923136d905290a419f295833cff1436cac24
andrew-mcgregor-mrp <andrew.mcgregor@mainstreamrp.com>
2018-07-20 20:33:18 +0100

    Edited cache to allow website to run.

--- fornerio/settings.py
+++ fornerio/settings.py
@@ -101,7 +101,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
